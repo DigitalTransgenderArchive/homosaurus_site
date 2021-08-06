ce6de90b45fad99bc7beb42f9cba3e2ced38a9b8
Steven Anderson <StevenCarlAnderson@gmail.com>
2021-08-05 22:28:42 -0700

    Add proxy for recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,7 @@
 Recaptcha.configure do |config|
   config.site_key  = Settings.recaptcha_site_key
   config.secret_key = Settings.recaptcha_secret_key
+  if Settings.dta_config["proxy_host"].present?
+    config.proxy = "http://#{Settings.homosaurus_config['proxy_host']}:#{Settings.homosaurus_config['proxy_port']}"
+  end
 end
