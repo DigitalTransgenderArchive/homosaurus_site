d143d360d6243de86c1c328fa6d46eaf72f1e67c
Steven Anderson <StevenCarlAnderson@gmail.com>
2023-12-18 16:16:52 -0800

    Remove additional unused seconbase models from previous migration to standalone application

--- app/models/homosaurus_closematch_lcsh.rb
+++
@@ -1,7 +0,0 @@
-class HomosaurusClosematchLcsh < SecondBase::Base
-  belongs_to :homosaurus_subject
-  belongs_to :lcsh_subject
-
-  self.table_name = "dta.homosaurus_closematch_lcsh"
-
-end
--- app/models/homosaurus_exactmatch_lcsh.rb
+++
@@ -1,6 +0,0 @@
-class HomosaurusExactmatchLcsh < SecondBase::Base
-  belongs_to :homosaurus_subject
-  belongs_to :lcsh_subject
-
-  self.table_name = "dta.homosaurus_exactmatch_lcsh"
-end
