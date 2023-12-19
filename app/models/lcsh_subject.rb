ab8d8a0f3e5502c87372d6f154384acafb2dda43
Steven Anderson <StevenCarlAnderson@gmail.com>
2023-12-18 16:14:11 -0800

    Remove unused old LCSH cach

--- app/models/lcsh_subject.rb
+++
@@ -1,9 +0,0 @@
-class LcshSubject < SecondBase::Base
-  self.table_name = "dta.lcsh_subjects"
-
-  serialize :alt_labels, Array
-  serialize :broader, Array
-  serialize :narrower, Array
-  serialize :related, Array
-
-end
