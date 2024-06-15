51842365362f1e461656887409fa6822464c5199
theadorabledev <theadorabledeveloper@gmail.com>
2024-06-15 20:40:30 +0000

    Migration bugfix

--- db/migrate/20240217062707_add_pendings.rb
+++ db/migrate/20240217062707_add_pendings.rb
@@ -3,7 +3,7 @@ class AddPendings < ActiveRecord::Migration[5.2]
     change_column_null :edit_requests, :term_id, true
     change_column_null :edit_requests, :version_release_id, true
     add_column :version_releases, :status, :text #Published, Pending, Unversioned
-    VersionRelease.all().update(status: "Published")
+    VersionRelease.update_all(status: "Published")
     # VersionRelease.all().each do |vr|
     #   vr.update(release_identifier: vr.release_identifier + ".0")
     # end
