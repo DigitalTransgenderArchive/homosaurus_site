5c5b016ca5e5c9ed2234a17ff5e29a50340b0ec8
theadorabledev <theadorabledeveloper@gmail.com>
2024-07-09 16:59:05 +0000

    Fixed release date bug

--- db/migrate/20240708163822_fix_dates_on_release.rb
+++ db/migrate/20240708163822_fix_dates_on_release.rb
@@ -1,10 +1,10 @@
 class FixDatesOnRelease < ActiveRecord::Migration[5.2]
   def up
     VersionRelease.update(release_date: nil)
-    change_column(:version_releases, :release_date, :timestamp)
+    change_column(:version_releases, :release_date, :datetime)
     VersionRelease.all.each do |vr|
-      if vr.edit_requests
-        vr.update(release_date: vr.edit_requests.pluck(:created_at).max)
+      if vr.edit_requests.count > 0
+        vr.update(release_date: vr.edit_requests.pluck(:created_at).max.to_datetime)
       end
     end
 
