03fe31bec8cda37251f9bf67b5518f4f61bfabf8
theadorabledev <theadorabledeveloper@gmail.com>
2024-07-16 20:35:18 +0000

    Updated to handle 3.5.1 properly.

--- db/migrate/20240217062707_add_pendings.rb
+++ db/migrate/20240217062707_add_pendings.rb
@@ -4,11 +4,11 @@ class AddPendings < ActiveRecord::Migration[5.2]
     change_column_null :edit_requests, :version_release_id, true
     add_column :version_releases, :status, :text #Published, Pending, Unversioned
     VersionRelease.update_all(status: "Published")
-    VersionRelease.find_by(id: 12).update(status: "Pending")
-    # VersionRelease.all().each do |vr|
-    #   vr.update(release_identifier: vr.release_identifier + ".0")
-    # end
+    execute <<-SQL 
+            UPDATE version_releases SET status = 'Pending' WHERE id = 12 ;
+    SQL
     EditRequest.where(parent_id: nil).each do |er|
+      status = (er.version_release.status == "Published") ? "approved"  : "pending"
       EditRequest.create!(:term_id => nil,
                           :created_at => er.created_at,
                           :version_release_id => nil,
@@ -19,13 +19,14 @@ class AddPendings < ActiveRecord::Migration[5.2]
     end
   end
   def down
+    EditRequest.where.not(parent_id: nil).destroy_all()
     change_column_null :edit_requests, :term_id, false
     change_column_null :edit_requests, :version_release_id, false
-    VersionRelease.where("release_identifier LIKE '%.%.%'").each do |vr|
-      vr.update(release_identifier: vr.release_identifier.slice(0, 3))
-    end
+    # VersionRelease.where("release_identifier LIKE '%.%.%'").each do |vr|
+    #   vr.update(release_identifier: vr.release_identifier.slice(0, 3))
+    # end
     remove_column :version_releases, :status
-    VersionRelease.where("id > 11").destroy_all()
+    VersionRelease.where("id > 12").destroy_all()
     #VersionRelease.where(id: 12).destroy_all()
     Vocabulary.where(id: 4).destroy_all()
   end
